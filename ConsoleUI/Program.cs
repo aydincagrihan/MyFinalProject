1ebb75e3df8c06ab6165f8bc3793503ef75c08ad
aydın tarım <cagrihanaydin@hotmail.com>
2021-03-05 16:37:14 +0300

    8.Gün güncellemeleri,Sisteme EntityFramework Eklendi ve refactoring işlemleri yapıldı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+	//SOLID
+	//Open Closed Principle-Yani sistemimize yeni bir özellik ekleyince diğer katmanlarımız
+	//iş süreçleri etkilenmedi bu örnekte localden-EF ye geçtik
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			ProductManager productManager = new ProductManager(new InMemoryProductDal());
-			foreach (var product in productManager.GetAll())
+			ProductManager productManager = new ProductManager(new EfProductDal());
+			foreach (var product in productManager.GetAllByCategoryId(3))//3.categorydeki ürünleri getir
 			{
 				Console.WriteLine(product.ProductName);
 
