6f05f142ddecc2522f65be7da8d8c6d0241221a7
aydın tarım <cagrihanaydin@hotmail.com>
2021-03-06 00:12:58 +0300

    9.gün sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,23 +12,28 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
-			//ProductTest();
+			ProductTest();
 			//Ioc,refactoring yapılacak yerler
+			//CategoryTest();
+		}
+
+		private static void CategoryTest()
+		{
 			CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 			foreach (var category in categoryManager.GetAll())
 			{
 				Console.WriteLine(category.CategoryName);
-				
 			}
 		}
 
 		private static void ProductTest()
 		{
 			ProductManager productManager = new ProductManager(new EfProductDal());
-			foreach (var product in productManager.GetAllByCategoryId(3)) //3.categorydeki ürünleri getir
+			foreach (var product in productManager.GetProductDetails()) //Product Name ve Category Name joinledik
 			{
-				Console.WriteLine(product.ProductName);
+				Console.WriteLine(product.ProductName + "/"+ product.CategoryName);
 			}
+
 		}
 	}
 }
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -3,6 +3,7 @@ using Core.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities.DTOs;
 
 namespace DataAccess.Abstract
 {/// <summary>
@@ -10,7 +11,7 @@ namespace DataAccess.Abstract
 /// </summary>
 	public interface IProductDal:IEntityRepository<Product>
 	{
-		
+	List<ProductDetailDto> GetProductDetails();
 
 	}
 }
