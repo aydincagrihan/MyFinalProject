a5747ae46139d6ac06f4e8bd338e7c50c26fbf03
aydın tarım <cagrihanaydin@hotmail.com>
2021-03-05 23:44:26 +0300

    Refactoring işlemleri yapıldı,9.gün

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+	public class CategoryManager : ICategoryService
+	{
+		/*
+		 * Business Layeri-DataAccesse bağımlı
+		 * ama DataAccess'te sonradan Ef den farklı Teknolojiler kullanılmak
+		 * istenirse,burda bağımlılığımızı minimaze ediyoruz
+		 * constructor injection!!!
+		 */
+		 ICategoryDal _categoryDal;
+		//interface(reference üzerinden bağımlılığı temsil ediyoruz burda)
+		 public CategoryManager(ICategoryDal categoryDal)
+		 {
+			 _categoryDal = categoryDal;
+		 }
+		public List<Category> GetAll()
+		{
+			//İş kodları
+			return _categoryDal.GetAll();
+
+		}
+
+		public Category GetById(int categoryId)
+		{
+			return _categoryDal.Get(c => c.CategoryId == categoryId);
+		}
+	}
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,13 +12,23 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			//ProductTest();
+			//Ioc,refactoring yapılacak yerler
+			CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+			foreach (var category in categoryManager.GetAll())
+			{
+				Console.WriteLine(category.CategoryName);
+				
+			}
+		}
+
+		private static void ProductTest()
+		{
 			ProductManager productManager = new ProductManager(new EfProductDal());
-			foreach (var product in productManager.GetAllByCategoryId(3))//3.categorydeki ürünleri getir
+			foreach (var product in productManager.GetAllByCategoryId(3)) //3.categorydeki ürünleri getir
 			{
 				Console.WriteLine(product.ProductName);
-
 			}
-			
 		}
 	}
 }
