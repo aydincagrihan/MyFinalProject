b1f44602cb0b361352b1b12548cf3b500560764b
aydın tarım <cagrihanaydin@hotmail.com>
2021-03-07 20:42:15 +0300

    WebAPI iyileştirmeleri yapıldı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -40,7 +40,7 @@ namespace Business.Concrete
 
 		public IDataResult<List<Product>> GetAll()
 		{
-			if (DateTime.Now.Hour == 14)
+			if (DateTime.Now.Hour == 3)
 			{
 				return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
 			}
